4e304eac77d586e532574e53c0860b72fbddbf6d
KarBo12 <85529229+KarBo12@users.noreply.github.com>
2021-06-08 18:37:45 +0300

    Update ProjectTickets.cpp

--- ProjectTickets.cpp
+++ ProjectTickets.cpp
@@ -10,20 +10,16 @@
 int main() {
 
 	Hall halls[5];
-	Event events[20];
-
-	std::cout << "Hi";
+	Event events[30];
 
 	halls[0].setHall(1, 10, 10);
-	halls[1].setHall(2, 10, 10);
-	halls[2].setHall(25, 10, 10);
-	halls[3].setHall(56, 10, 10);
-	halls[4].setHall(4, 10, 10);
+	halls[1].setHall(2, 11, 11);
+	halls[2].setHall(3, 12, 12);
+	halls[3].setHall(4, 13, 13);
+	halls[4].setHall(5, 14, 14);
 
-	Run runi(events, halls);
-	runi.run();
-	
-
+	Run theatre(events, halls);
+	theatre.run();
 
 	return 0;
 }
